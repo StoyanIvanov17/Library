4cf9913248ec9d9110e46757a55be31bd4f7e18c
StoyanIvanov17 <stoyanivanovmail@gmail.com>
2024-10-22 17:16:47 +0300

    made the email sending view asynchronously

--- library/library/lb_accounts/views.py
+++ library/library/lb_accounts/views.py
@@ -1,9 +1,6 @@
-import asyncio
-import smtplib
 import uuid
-from email.mime.text import MIMEText
 
-from django.conf import settings
+from asgiref.sync import sync_to_async
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
@@ -12,11 +9,13 @@ from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.urls import reverse_lazy, reverse
 from django.views import generic as views
-from django.contrib.auth import views as auth_views, login, logout
+from django.contrib.auth import views as auth_views, login, logout, get_user_model
 
 from library.lb_accounts.forms import LibraryUserCreationForm, LibraryProfileForm
 from library.lb_accounts.models import LibraryProfile
 from library.lb_accounts.utils.library_card_number_generator import generate_library_card_number
+
+UserModel = get_user_model()
 
 
 class SignInUserView(auth_views.LoginView):
@@ -83,48 +82,28 @@ class SignUpUserView(views.CreateView):
 
 
 class VerifyEmailView(views.View):
-    def get(self, request, token):
+    async def get(self, request, token):
         try:
-            profile = LibraryProfile.objects.get(verification_token=token)
+            # Use sync_to_async to fetch the profile and user
+            profile = await sync_to_async(LibraryProfile.objects.get)(verification_token=token)
 
-            user = profile.user
+            user = await sync_to_async(UserModel.objects.get)(libraryprofile=profile)
             user.is_active = True
-            user.save()
+            await sync_to_async(user.save)()
+
             profile.verified = True
-            profile.save()
+            await sync_to_async(profile.save)()
 
-            login(request, profile.user)
+            await sync_to_async(login)(request, user)
 
-            messages.success(request, "Your email has been verified! You can now proceed to your profile.")
+            # Use sync_to_async for messages
+            await sync_to_async(messages.success)(request,
+                                                  "Your email has been verified! You can now proceed to your profile.")
             return redirect('registration profile')
-        except LibraryProfile.DoesNotExist:
-            messages.error(request, "Invalid verification link.")
-            return redirect('home page')
 
-# class SignUpUserView(views.CreateView):
-#     template_name = 'accounts/signup_user.html'
-#     form_class = LibraryUserCreationForm
-#     success_url = reverse_lazy('home page')
-#
-#     def form_valid(self, form):
-#         user = form.save(commit=False)
-#         user.library_card_number = generate_library_card_number()
-#         user.save()
-#
-#         try:
-#             profile = LibraryProfile.objects.create(
-#                 user=user,
-#                 email=user.email,
-#             )
-#             login(self.request, user)
-#             return redirect('registration profile')
-#         except IntegrityError:
-#
-#             try:
-#                 profile = LibraryProfile.objects.get(user=user)
-#                 return redirect('account details', pk=profile.pk)
-#             except LibraryProfile.DoesNotExist:
-#                 return redirect('home page')
+        except LibraryProfile.DoesNotExist:
+            await sync_to_async(messages.error)(request, "Invalid verification link.")
+            return redirect('home page')
 
 
 class LibraryProfileCreateView(LoginRequiredMixin, views.UpdateView):
@@ -152,7 +131,7 @@ class AccountDetailsView(views.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        profile = self.get_object()  # current profile
+        profile = self.get_object()
         context['saved_items'] = profile.saved_items.all()
         context['saved_events'] = profile.saved_events.all()
         return context
@@ -167,23 +146,3 @@ class AccountUpdateView(views.UpdateView):
         return reverse("account details", kwargs={
             "pk": self.object.pk,
         })
-
-
-async def send_email(subject, message, recipient_list):
-    loop = asyncio.get_event_loop()
-    await loop.run_in_executor(None, _send_email_sync, subject, message, recipient_list)
-
-
-def _send_email_sync(subject, message, recipient_list):
-    msg = MIMEText(message)
-    msg['Subject'] = subject
-    msg['From'] = settings.DEFAULT_FROM_EMAIL
-    msg['To'] = ", ".join(recipient_list)
-
-    try:
-        with smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT) as server:
-            server.starttls()  # Enable TLS
-            server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
-            server.sendmail(settings.DEFAULT_FROM_EMAIL, recipient_list, msg.as_string())
-    except Exception as e:
-        print(f"Error sending email: {e}")
