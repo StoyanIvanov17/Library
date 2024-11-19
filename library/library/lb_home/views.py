7822fe0500880200ce9258b7b8f02c20b9123a87
StoyanIvanov17 <stoyanivanovmail@gmail.com>
2024-11-19 19:17:56 +0200

    finished the home page

--- library/library/lb_home/views.py
+++ library/library/lb_home/views.py
@@ -16,6 +16,7 @@ class HomePageView(views.TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['profile'] = self.request.user
+        context['bestselling_authors'] = BestAuthors.objects.all()
         return context
 
 
