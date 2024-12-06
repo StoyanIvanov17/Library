18782d0d6b839bf894f5384fb8bc766831c5c825
StoyanIvanov17 <stoyanivanovmail@gmail.com>
2024-12-06 22:37:07 +0200

    .

--- library/wsgi.py
+++ library/wsgi.py
@@ -8,13 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
-
-# Get the PORT environment variable
-port = os.getenv('PORT', '8000')
-os.environ.setdefault('PORT', port)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library.settings')
 
 application = get_wsgi_application()
-
